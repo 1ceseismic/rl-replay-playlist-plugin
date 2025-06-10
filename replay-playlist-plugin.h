b70fe71507f6635f96545caebc63fa9f5863d98e
joeru <joeru.dev@gmail.com>
2025-06-11 00:24:21 +1200

    fix wrong methods, BM_LOGS and a bunch of identifiers issues still present

--- replay-playlist-plugin.h
+++ replay-playlist-plugin.h
@@ -1,26 +1,54 @@
 #pragma once
 
-#include "GuiBase.h"
 #include "bakkesmod/plugin/bakkesmodplugin.h"
 #include "bakkesmod/plugin/pluginwindow.h"
 #include "bakkesmod/plugin/PluginSettingsWindow.h"
 
-#include "version.h"
-constexpr auto plugin_version = stringify(VERSION_MAJOR) "." stringify(VERSION_MINOR) "." stringify(VERSION_PATCH) "." stringify(VERSION_BUILD);
+#include <string>
+#include <vector>
+#include <filesystem>
+#include <map>
 
+// Forward declaration
+class ReplayPlaylistPlugin;
 
-class replay_playlist_plugin: public BakkesMod::Plugin::BakkesModPlugin
-	//,public SettingsWindowBase // Uncomment if you wanna render your own tab in the settings menu
-	//,public PluginWindowBase // Uncomment if you want to render your own plugin window
+// Struct to hold information about each playlist type and its CVar
+struct PlaylistTypeSetting {
+    std::string cvarName;
+    std::string displayName;
+    int defaultValue;
+    std::shared_ptr<int> cvarValuePtr; // Pointer to the CVar's value
+
+    PlaylistTypeSetting(std::string name, std::string display, int defVal)
+        : cvarName("rpp_max_replays_" + name), displayName(display), defaultValue(defVal) {}
+};
+
+class ReplayPlaylistPlugin : public BakkesMod::Plugin::BakkesModPlugin, public BakkesMod::Plugin::PluginSettingsWindow
 {
-
-	//std::shared_ptr<bool> enabled;
-
-	//Boilerplate
-	void onLoad() override;
-	//void onUnload() override;
-
 public:
-	//void RenderSettings() override; // Uncomment if you wanna render your own tab in the settings menu
-	//void RenderWindow() override; // Uncomment if you want to render your own plugin window
+    // BakkesModPlugin
+    void onLoad() override;
+    void onUnload() override;
+
+    // PluginSettingsWindow
+    void RenderSettings() override;
+    std::string GetPluginName() override;
+    void SetImGuiContext(uintptr_t ctx) override;
+
+private:
+    // Methods
+    void CreatePluginDataDirectory();
+    void RegisterCVars();
+    void LoadConfig();
+    void EventMatchEnded(const std::string& eventName); // Hook for match ended
+
+    // Member variables
+    std::filesystem::path pluginDataDir;
+    std::filesystem::path autosavedReplaysDir;
+
+    // Map to store playlist type settings and their CVars
+    std::map<std::string, PlaylistTypeSetting> playlistSettings;
+
+    // Helper to easily get CVar values
+    int GetMaxReplaysForPlaylist(const std::string& playlistKeyName);
 };
